623e9ff725df1421d9f292578fbd8da4f38126ad
NateGrowcock <98107944+NateGrowcock@users.noreply.github.com>
2022-03-03 10:02:25 -0600

    Revised question 3

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,10 +5,14 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 
+
+
+
 const Calendar = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />  
+    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} /> 
+    
   );
 };
 
